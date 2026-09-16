dc5800608abe8fad3e72005fc4e952a80789f712
adr34405 <adr34405@educastur.es>
2026-09-16 10:47:00 +0200

    Add files via upload

---
+++ ejer 1.1.js
@@ -0,0 +1,10 @@
+const nombre = "Aday"
+let edad = 19
+let tieneMascota = true
+edad = 20
+tieneMascota = false
+console.log(nombre)
+console.log(edad)
+console.log(tieneMascota)
+const frase = `${nombre} tiene ${edad} años y tiene mascota.`
+console.log(frase)
